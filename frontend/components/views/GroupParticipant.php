4f836b4bd0edb606f0d633dff8f8185558abbc47
RAdKuzur <drive16052003@gmail.com>
2025-04-06 15:16:12 +0400

    fix group-participant widget

--- frontend/components/views/GroupParticipant.php
+++ frontend/components/views/GroupParticipant.php
@@ -10,9 +10,19 @@
 /* @var $groupCheckOption */
 /* @var $groupParticipantOption */
 ?>
+<?php var_dump($model); ?>
 <?= $this->render('_groups_grid', [
     'dataProvider' => $dataProviderGroup,
     'model' => $model,
     'groupCheckOption' => $groupCheckOption,
+]);
+?>
+<?php var_dump($model); ?>
+<?= $this->render('_group-participant_grid', [
+    'dataProvider' => $dataProviderParticipant,
+    'model' => $model,
+    'nomenclature' => $nomenclature,
+    'transferGroups' => $transferGroups,
+    'groupParticipantOption' => $groupParticipantOption,
 ]);
 ?>

f22b8b27a1571467ce3c280a11bd3c0bae298111
RAdKuzur <drive16052003@gmail.com>
2025-04-06 13:53:43 +0400

    fix order-training

--- frontend/controllers/order/OrderTrainingController.php
+++ frontend/controllers/order/OrderTrainingController.php
@@ -136,6 +136,7 @@ class OrderTrainingController extends DocumentController
             NULL,
             NULL,
         );
+        var_dump('Ok! 1');
         $post = Yii::$app->request->post();
         if ($model->load($post)) {
             $this->documentOrderService->getPeopleStamps($model);
@@ -161,6 +162,7 @@ class OrderTrainingController extends DocumentController
                 return $this->redirect(Yii::$app->request->referrer ?: ['create']);
             }
         }
+        var_dump('Ok! 2');
         return $this->render('create', [
             'model' => $model,
             'people' => $form->people,

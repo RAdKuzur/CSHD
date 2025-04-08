c078ea238368e77c1efe14dda6053a2d7d71f4aa
unknown <Admin@T6-3U.RST.EDU>
2025-04-08 15:16:46 +0400

    document out fixes

--- common/services/general/files/FileService.php
+++ common/services/general/files/FileService.php
@@ -65,8 +65,7 @@ class FileService
 
         var_dump(Yii::$app->basePath . $finalPath . $filename);
         if ($file) {
-            $file->saveAs(Yii::$app->basePath . $finalPath . $filename);
-            var_dump($file->getHasError());die;
+            var_dump($file->saveAs(Yii::$app->basePath . $finalPath . $filename));die;
             if ($file->size > self::FILE_LIMIT){
                 //загрузка на Диск
                 $this->uploadDisk($finalPath, $filename, $params);

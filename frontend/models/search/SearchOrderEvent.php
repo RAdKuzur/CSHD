ccb04aba1937365ca0949c393088c803de243762
alexkriak <lemon1and3cherry@gmail.com>
2025-04-09 17:30:51 +0400

    update search

--- frontend/models/search/SearchOrderEvent.php
+++ frontend/models/search/SearchOrderEvent.php
@@ -87,7 +87,7 @@ class SearchOrderEvent extends OrderSearch implements SearchInterfaces
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
-            'sort'=> ['defaultOrder' => ['order_number' => SORT_DESC, 'order_copy_id' => SORT_DESC, 'order_postfix' => SORT_DESC]]
+            'sort'=> ['defaultOrder' => ['order_date' => SORT_DESC, 'order_number' => SORT_DESC, 'order_copy_id' => SORT_DESC, 'order_postfix' => SORT_DESC]]
         ]);
 
         $this->sortAttributes($dataProvider);

5480982c831a5ba37ca53a712c869a7114e6b809
RAdKuzur <drive16052003@gmail.com>
2025-04-11 22:47:34 +0400

    fix order generate

--- frontend/models/work/dictionaries/PositionWork.php
+++ frontend/models/work/dictionaries/PositionWork.php
@@ -3,10 +3,17 @@
 namespace frontend\models\work\dictionaries;
 
 use common\models\scaffold\Position;
+use morphos\Russian\NounDeclension;
 
 class PositionWork extends Position
 {
     public function getPositionName(){
         return $this->name;
+    }
+    public function getGenitivePositionName()
+    {
+        $parts = explode(' ', $this->name);
+        $parts[0] = NounDeclension::getCase($parts[0], 'родительный');
+        return implode(' ', $parts);
     }
 }

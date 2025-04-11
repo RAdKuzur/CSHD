8e39a33548a0076fde1e27a83a64a1a5f9d68c11
unknown <Admin@T6-3U.RST.EDU>
2025-04-11 11:41:32 +0400

    fix

--- frontend/models/work/educational/training_group/TrainingGroupWork.php
+++ frontend/models/work/educational/training_group/TrainingGroupWork.php
@@ -149,7 +149,8 @@ class TrainingGroupWork extends TrainingGroup implements FileInterface
         if (count($sameNameGroups) == 1) {
             preg_match($pattern, $sameNameGroups[0]->number, $matches);
             $number1 = $matches[1];
-            var_dump($sameNameGroups[0]->number);
+            var_dump($matches[0]);
+            var_dump($matches[1]);
             $addCode = (string)((int)$number1 + 1);
         } else {
             for ($i = 0; $i < count($sameNameGroups) - 1; $i++) {

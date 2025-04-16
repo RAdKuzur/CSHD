6973206c3d53232b0a15be656b27637df3ac2bd3
RAdKuzur <drive16052003@gmail.com>
2025-04-16 19:01:13 +0400

    fix

--- common/helpers/files/filenames/DocumentInFileNameGenerator.php
+++ common/helpers/files/filenames/DocumentInFileNameGenerator.php
@@ -52,9 +52,7 @@ class DocumentInFileNameGenerator implements FileNameGeneratorInterface
         $lastDocFile = $this->filesRepository->getLastFile($object::tableName(), $object->id, FilesHelper::TYPE_DOC);
         /** @var FilesWork $lastDocFile */
         if ($lastDocFile) {
-            var_dump('OK!!!!');
             preg_match('/Ред(\d+)_/', $lastDocFile->filepath, $matches);
-            var_dump($lastDocFile->filepath, $matches);
             return (int)$matches[1];
         }
 
@@ -66,7 +64,7 @@ class DocumentInFileNameGenerator implements FileNameGeneratorInterface
         $lastAppFile = $this->filesRepository->getLastFile($object::tableName(), $object->id, FilesHelper::TYPE_APP);
         /** @var FilesWork $lastAppFile */
         if ($lastAppFile) {
-            preg_match('/Приложение(\d+)_/', basename($lastAppFile->filepath), $matches);
+            preg_match('/Приложение(\d+)_/', $lastAppFile->filepath, $matches);
             return (int)$matches[1];
         }
 

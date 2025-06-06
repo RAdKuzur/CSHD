0c0e2fab8671da4c58568ca22c8fed188ee08aa6
EscBar <timuryyaga@gmail.com>
2025-06-06 12:59:35 +0400

    mail validation handle exceptions

--- frontend/components/wizards/CertificateWizard.php
+++ frontend/components/wizards/CertificateWizard.php
@@ -133,39 +133,32 @@ class CertificateWizard
     public static function sendCertificateToEmail(CertificateWork $certificate)
     {
         $name = self::downloadCertificate($certificate, $certificate->trainingGroupParticipantWork, self::DESTINATION_SERVER, Yii::$app->basePath . '/download/' . Yii::$app->user->identity->getId() . '_temp_certificates/');
+        try {
+            $result = Yii::$app->mailer->compose()
+                ->setFrom('noreply@schooltech.ru')
+                ->setTo($certificate->trainingGroupParticipantWork->participant->email)
+                ->setSubject('Сертификат об успешном прохождении программы ДО')
+                ->setHtmlBody('Сертификат находится в прикрепленном файле.<br><br><br>Пожалуйста, обратите внимание, что это сообщение было сгенерировано и отправлено в автоматическом режиме. Не отвечайте на него. По всем вопросам обращайтесь по телефону 44-24-28 (единый номер).')
+                ->attach(Yii::$app->basePath . '/download/' . Yii::$app->user->identity->getId() . '_temp_certificates/' . $name . '.pdf')
+                ->send();
 
-        $email = $certificate->trainingGroupParticipantWork->participant->email;
-
-        // Проверка формата email
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            //Yii::error("Некорректный email: $email");
+            if ($result) {
+                $certificate->recordEvent(new CertificateSetStatusEvent($certificate->id, CertificateWork::STATUS_SEND), CertificateWork::className());
+            } else {
+                $certificate->recordEvent(new CertificateSetStatusEvent($certificate->id, CertificateWork::STATUS_ERR_SEND), CertificateWork::className());
+            }
+        } catch (\Swift_RfcComplianceException $e) {
+            Yii::error('Ошибка RFC (почта не существует) при отправке письма: ' . $e->getMessage(), __METHOD__);
             $certificate->recordEvent(new CertificateSetStatusEvent($certificate->id, CertificateWork::STATUS_ERR_SEND), CertificateWork::className());
-            return false;
-        }
-
-        // 2. Проверка MX-записи (существует ли домен)
-        $domain = explode('@', $email)[1] ?? '';
-        if (!checkdnsrr($domain, 'MX')) {
-            //Yii::error("Домен почты не существует: $email");
-            $certificate->recordEvent(new CertificateSetStatusEvent($certificate->id, CertificateWork::STATUS_ERR_SEND), CertificateWork::className());
-            return false;
-        }
-
-        $result = Yii::$app->mailer->compose()
-            ->setFrom('noreply@schooltech.ru')
-            ->setTo($certificate->trainingGroupParticipantWork->participant->email)
-            ->setSubject('Сертификат об успешном прохождении программы ДО')
-            ->setHtmlBody('Сертификат находится в прикрепленном файле.<br><br><br>Пожалуйста, обратите внимание, что это сообщение было сгенерировано и отправлено в автоматическом режиме. Не отвечайте на него. По всем вопросам обращайтесь по телефону 44-24-28 (единый номер).')
-            ->attach(Yii::$app->basePath . '/download/' . Yii::$app->user->identity->getId() . '_temp_certificates/' . $name . '.pdf')
-            ->send();
-        if ($result) {
-            $certificate->recordEvent(new CertificateSetStatusEvent($certificate->id, CertificateWork::STATUS_SEND), CertificateWork::className());
-        }
-        else {
+        } catch (\Throwable $e) {
+            // Логируем любые другие возможные исключения
+            Yii::error('Ошибка при отправке письма: ' . $e->getMessage(), __METHOD__);
             $certificate->recordEvent(new CertificateSetStatusEvent($certificate->id, CertificateWork::STATUS_ERR_SEND), CertificateWork::className());
         }
+
         $certificate->releaseEvents();
 
-        return $result;
+        return isset($result) ? $result : false;
+
     }
 }

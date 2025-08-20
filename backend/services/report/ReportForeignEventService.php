7f6c3171eacaf2d90cebbebd0d9b6af108e081be
RAdKuzur <drive16052003@gmail.com>
2025-08-20 13:50:12 +0400

    fix foreign-event-report

--- backend/services/report/ReportForeignEventService.php
+++ backend/services/report/ReportForeignEventService.php
@@ -47,6 +47,7 @@ class ReportForeignEventService implements ForeignEventServiceInterface
         $actsQuery = $this->builder->joinWith($actsQuery, 'foreignEventWork');
         $actsQuery = $this->builder->joinWith($actsQuery, 'actParticipantBranchWork');
         $actsQuery = $this->builder->joinWith($actsQuery, 'participantAchievementWork');
+        $actsQuery = $this->builder->joinWith($actsQuery, 'squadParticipantWork');
         $actsQuery = $this->builder->filterByBranches($actsQuery, $branches);
         $actsQuery = $this->builder->filterByFocuses($actsQuery, $focuses);
         $actsQuery = $this->builder->filterByAllowRemote($actsQuery, $allowRemotes);

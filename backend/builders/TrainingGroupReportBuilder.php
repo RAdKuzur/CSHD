59351a1396c0c4f2d6eb7a023adad4c33541f5a7
EscBar <timuryyaga@gmail.com>
2026-03-17 16:12:37 +0400

    Fix man-hours (rufat isset zachem?)

--- backend/builders/TrainingGroupReportBuilder.php
+++ backend/builders/TrainingGroupReportBuilder.php
@@ -140,8 +140,8 @@ class TrainingGroupReportBuilder
         }
         if (in_array(ManHoursReportForm::PARTICIPANT_START_BEFORE_FINISH_AFTER, $types)) {
             $conditions[] = QueryHelper::getGroupsStartBeforeFinishAfterDates($date1, $date2);
-        }
-        if (in_array(ManHoursReportForm::PARTICIPANT_WITH_BENEFITS_BY_SVO, $types) || !isset($array)) {
+        } //|| !isset($array)
+        if (in_array(ManHoursReportForm::PARTICIPANT_WITH_BENEFITS_BY_SVO, $types)) {
             $conditions[] = QueryHelper::getGroupsStartBeforeFinishInDates($date1, $date2);
             $conditions[] = QueryHelper::getGroupsStartInFinishInDates($date1, $date2);
             $conditions[] = QueryHelper::getGroupsStartInFinishAfterDates($date1, $date2);
